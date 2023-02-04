6ad623a07e004edb0c7c23024d2a636f70070b79
nicki1002 <nicole18hernandez@hotmail.com>
2023-02-04 11:09:43 -0700

    Practica 02

---
+++ src/main/java/com/Sitio_turistico/SitioTuristicoApplication.java
@@ -0,0 +1,13 @@
+package com.Sitio_turistico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitioTuristicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitioTuristicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sitio_turistico/SitioTuristicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sitio_turistico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitioTuristicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
